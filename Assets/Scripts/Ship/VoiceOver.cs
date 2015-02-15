77d8f8dcab9babe8cac5ec8d55b9529a1dcc9cbe
Davidizm <skullmaru@live.com>
2015-02-15 13:17:05 -0600

    Added Tutorial Audio
    1. Tutorial Audio is in there and it plays on the MinionSpirit object in
    the hierarchy
    
    2. Changed the color of the 'WrongScore' object in the hierarchy

---
+++ Assets/Scripts/Ship/VoiceOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoiceOver : MonoBehaviour {
+
+    public AudioClip[] voiceOver = new AudioClip[10];
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PlayTutorial()); // plays tutorial clips in array only once
+	}	
+
+   IEnumerator PlayTutorial() // Plays audio for tutorial clips
+    {
+       foreach (AudioClip tutorialClip in voiceOver)
+       {
+           audio.clip = tutorialClip;
+           audio.Play();
+           yield return new WaitForSeconds(tutorialClip.length);
+       }
+    }
+}
