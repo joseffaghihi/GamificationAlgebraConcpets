da1410ecce1f6445d64de91dd199e3d5e531b6bd
SibTiger <Nicholas.Gautier.Tiger@GMail.com>
2015-02-20 03:07:38 -0600

    Implemented Tutorial Skip Button
    Added the ability to skip the tutorial; although there is still one
    issue where the tutorial mode will not hang up until the IEnumerator is
    finished.  I'll try to fix this soon, once I can think of a proper fix.

---
+++ Assets/Scripts/Ship/TutorialSkipButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialSkipButton : MonoBehaviour
+{
+    /*                    TUTORIAL SKIP BUTTON
+     * This script, being simple, allows the player to skip the entire tutorial.  This is done by sending a signal to the tutorial to 'stop' its execution.
+     *  
+     * GOALS:
+     *  When activated (or true), stop the tutorial and start the game or the next part of the game.
+     */
+
+
+    // Declarations
+    // -------------
+    // Script References
+        public VoiceOver tutorial;
+    // ----
+
+
+
+    // When the user clicks this object, skip the tutorial.
+    void OnMouseDown()
+    { 
+        tutorial.ToggleSkip(true);
+    } // End of OnMouseDown
+} // End of Class
--- Assets/Scripts/Ship/VoiceOver.cs
+++ Assets/Scripts/Ship/VoiceOver.cs
@@ -9,6 +9,8 @@ public class VoiceOver : MonoBehaviour
     public AudioClip[] voiceOver = new AudioClip[10];
     // This variable triggers the game state of wither or not the tutorial is activated.
     private bool tutorialMode;
+    // Skip Signal
+    private bool skip = false;
     // ----
 
 
@@ -26,15 +28,34 @@ public class VoiceOver : MonoBehaviour
     {
        foreach (AudioClip tutorialClip in voiceOver)
        {
-           audio.clip = tutorialClip;
-           audio.Play();
-           yield return new WaitForSeconds(tutorialClip.length);
+           // Run tutorial
+           if (skip == false)
+           {
+               audio.clip = tutorialClip;
+               audio.Play();
+               yield return new WaitForSeconds(tutorialClip.length);
+           }
+           
+           // Skip tutorial; wait for 2 seconds for prepare time.
+           else
+               yield return new WaitForSeconds(2);
        } // End foreach
 
        // turn off the tutorial mode
        tutorialMode = false;
 
     } // End of PlayTutorial
+
+
+
+    // This function, when called by another skip, will toggle the tutorial skip variable.
+    public void ToggleSkip(bool toggle)
+   {
+       if (toggle == true)
+           skip = true;
+       else
+           skip = false;
+   } // End of ToggleSkip
 
 
 
