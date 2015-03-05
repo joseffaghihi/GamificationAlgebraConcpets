23ef73f25f761f0e353c4fc79615517839961a9c
SibTiger <Nicholas.Gautier.Tiger@GMail.com>
2015-03-04 19:21:35 -0600

    Imported Scale project.
    Imported the latest version of the scale project into the main repo.
    
    Note: This project also supports Unity 5.

---
+++ Assets/Scales/MyScripts/Balance/Physics/ChangeForce.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeForce : MonoBehaviour {
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    public void setPosition(GameObject weight)
+    {
+        //Store the Weights intial Information
+        originalPosition = weight.transform.position;
+        originalRotation = weight.transform.rotation;
+    }
+
+    //Reset the Gameobject
+    public void Reset(GameObject weight)
+    {
+        //Sets the objects position to it's default
+        weight.transform.position = originalPosition;
+        weight.transform.rotation = originalRotation;
+
+        //Execute if the gameobject is a rigidbody
+        //Sets the rigidbodie's forces to their null state
+        if (weight.GetComponent<Rigidbody>() != null)
+        {
+            weight.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            weight.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            weight.GetComponent<Rigidbody>().useGravity = false;
+            weight.GetComponent<ConstantForce>().force = Vector3.down * 0;
+        }
+    }
+
+    //Execute on an in-game button click
+    //Enables forces to make the Gameobject fall
+    public void OnButtonClicked()
+    {
+        GetComponent<ConstantForce>().force = Vector3.down * 100;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
---
+++ Assets/Scales/MyScripts/Balance/TextMesh/Equation.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/*
+//TYPES OF EQUATIONS
+// 1 - linear
+// 2 - quadratic (to be implemented)
+ */
+
+public class Equation
+{
+	//***Declaration***
+	//=================
+
+	private int NumA; //Stores all randomly generated numbers for the equations
+	private int solution; //Stores the solution to the equation
+	private string type; //Stores the type of equation (ex. linear or quadratic)
+
+	//***Constructor***
+	//=================
+
+	public Equation(string type)
+	{
+		this.type = type; //Set the type of equation
+
+		//Initialize Equation based on the type
+		InitializeEquation ();
+	}
+
+	//***Public Functions***
+	//======================
+
+	//Output the Equation
+	public Text Output(Text equation)
+	{
+		//Output based on the type of equation
+		switch (type) 
+		{
+		case "linear":
+			if (NumA > 0)
+				equation.text = "x + " + NumA + " = 0";
+			else if (NumA < 0)
+				equation.text = "x - " + Mathf.Abs(NumA) + " = 0";
+			break;
+			
+		case "quadratic":
+			SetQuadratic();
+			break;
+			
+		default:
+			Debug.Log("Error: Type of Equation does not exist. (Outputting the Equation)");
+			break;
+		}
+
+		return equation;
+	}
+
+	//Returns the solution
+	public int GetSolution()
+	{
+		return solution;
+	}
+
+	//Returns the First Random Number
+	public int GetNumA()
+	{
+		return NumA;
+	}
+
+	//***Private Functions***
+	//=======================
+
+	//Compute the Solution based on the type of equation
+	private int ComputeSolution()
+	{
+		return solution;
+	}
+
+	//Set all the neccessary information for a linear equation
+	private void SetLinear()
+	{
+		NumA = Random.Range(-9, 9); //Generate random number
+		if (NumA == 0) 
+		{
+			NumA = Random.Range (1, 9);
+		}
+
+		solution = -NumA; //Set solution
+	}
+
+	//Set all the neccessary information for a quadratic equation
+	private void SetQuadratic()
+	{
+	}
+
+	//Get the necessary information for the type of equation
+	private void InitializeEquation()
+	{
+		switch (type) 
+		{
+		case "linear":
+			SetLinear();
+			break;
+
+		case "quadratic":
+			SetQuadratic();
+			break;
+
+		default:
+			Debug.Log("Error: Type of Equation does not exist (Initializing the Equation)");
+			break;
+		}
+	}
+}
