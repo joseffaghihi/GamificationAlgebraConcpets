1484bf3f903f186370865658c66cfa26bcf9e3f7
lipkame <melvin.lipka@gmx.de>
2015-03-18 00:20:38 -0500

    Updated Treadmill Organizational Structure
    Changed some of the game objects and components. For example, I put the
    Rigidbody component on an empty parent instead of on the same object
    with the animator component.

--- Assets/TheTreadmill/Scripts/Player/PlayerController.cs
+++ Assets/TheTreadmill/Scripts/Player/PlayerController.cs
@@ -11,8 +11,7 @@ public class Boundary
 public class PlayerController : MonoBehaviour
 {
     //Speed of the player
-    public float forwardSpeed = 10.0f; //Speed of player when going forward
-    public float backSpeed = 15.0f; //Speed of player when going backwards
+    public float forwardSpeed = 20.0f; //Speed of player when going forward
 	public float jumpSpeed = 10.0f; //Speed of the Jump
 
     //Tilt of the player
@@ -21,32 +20,32 @@ public class PlayerController : MonoBehaviour
 
     public Boundary boundary; //Holds boundaries of player movement area
 
-	bool onGround = true; //Checks to see if the player is on the Ground
+	private bool onGround = true; //Checks to see if the player is on the Ground
+
+    private float maxSpeed = 1000.0f;
 
     void FixedUpdate()
     {
         //Get User Input
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        float Jump = Input.GetAxis("Jump");
 
 		//Set Speed accrodingly
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); //Set player movement
-
-        if(moveVertical < 0) //If running backwards
-            GetComponent<Rigidbody>().velocity = movement * backSpeed; //Set player velocity based on the movement and backwards speed
-        else
-            GetComponent<Rigidbody>().velocity = movement * forwardSpeed; //Set player velocity based on the movement and forward speed
+        GetComponent<Rigidbody>().velocity = movement * forwardSpeed; //Set player velocity based on the movement and speed
 
 		//Check 'Y' coordinate (to see if player is on the ground)
-		if (GetComponent<Rigidbody>().position.y <= boundary.yMin)
+		if (GetComponent<Rigidbody>().position.y <= boundary.yMin /*+ errorRange*/)
 			onGround = true;
 
 		//Jump
 		if (Input.GetKeyDown(KeyCode.Space) && onGround) 
 		{
-			Jump();
+            //GetComponent<Rigidbody>().AddForce(10.0f * transform.forward + new Vector3(0, 4, 0), ForceMode.VelocityChange);
+            //GetComponent<Rigidbody>().AddForce(transform.up * 500);
 			onGround = false;
-		}   
+		}
 
         //Check boundary limits
         GetComponent<Rigidbody>().position = new Vector3
@@ -64,7 +63,7 @@ public class PlayerController : MonoBehaviour
 	void Jump() 
 	{
         //GetComponent<Rigidbody>().position += new Vector3(0.0f, jumpSpeed * Time.deltaTime, 0.0f);
-		//GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed * 100);	
+		GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed * 100);	
 
         //Play jump animation
 	}
