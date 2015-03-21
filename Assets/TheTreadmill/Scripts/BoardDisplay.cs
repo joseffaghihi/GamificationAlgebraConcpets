16ec8cb17b794c8af52e25e166caf2975dc9d21a
lipkame <melvin.lipka@gmx.de>
2015-03-21 10:15:43 -0500

    Synced game board
    The game board now works for the treadmill game.
    (I will change the way it is called later on in order to make it more
    efficient instead of updating it every second.)

---
+++ Assets/TheTreadmill/Scripts/BoardDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BoardDisplay : MonoBehaviour 
+{
+
+    GameControl gameController = new GameControl();
+
+    /**
+     * Updating text will later on be moved to a public function to avoid constant updating thereby saving cpu power making it more efficient
+     */
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        GetComponent<Text>().text = "Lives Left: " + gameController.lives  +
+                                    "\nSpeed: " + gameController.treadmillSpeed;
+	}
+}
