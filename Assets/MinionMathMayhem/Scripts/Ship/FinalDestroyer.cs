a63a19f3f249ff0450fe028ea9e83970943d65bf
SibTiger <Nicholas.Gautier.Tiger@GMail.com>
2015-03-22 00:49:55 -0500

    Code Revision: FinalDestroyer
    The FinalDestroyer will now fetch the actor's uniquely assigned number
    and cache it.  Classes can request the number by accessing the
    'ActorIdentity' accessor.

--- Assets/MinionMathMayhem/Scripts/Ship/FinalDestroyer.cs
+++ Assets/MinionMathMayhem/Scripts/Ship/FinalDestroyer.cs
@@ -1,231 +1,52 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
 public class FinalDestroyer : MonoBehaviour
 {
 
     /*                      FINAL DESTROYER
-     * This class handles with determining if the minion's that collided with the 'exit' actor, is the correct or incorrect answer to the given problem.
-     *  If the answer is correct, this script will handle with the routine algorithm of updating the game state.
+     * When the actor triggers the exit object, this class will simply cache the actor's identity number and kill the actor from the scene.
+     *   The cached identity will be used from another
      *  
      * GOALS:
-     *  Check the minion's self-assigned number and compare against the current quadratic equation index
-     *  Manage when the equation should be created
-     *  Determining if the answer is correct or incorrect
-     *  Toggle the spawner's; given in specific condition
-     *  Clear scene of the minions
+     *  Fetch the actor's uniquly self-assigned number and cache it.
+     *  Destroy the actor
      */
 
 
-
-    // Declarations
-    // -------------
-    // Link to other scripts
-        public ProblemBox problemBox;
-		public GameObject whatIsText;
-		public GameObject eventLetterText;
-        public LetterBox letterbox;
-        public MinionsSpawnWait minionSpawnWait;
-        public Text letterBox;
-	    //public Text whatIsText;
-        public Score score;
-        public GameState gameState;
-    // Toggle the minion spawner's
-        public bool activateSpawner;
-    // Wait timer for the minion check
-        public int waitTimer = 5;
-    // Lock the function from executing; work around gross hack
-        private bool lockCheckFunction;
-    // Sounds
-	    public AudioSource gameSounds;
-	    public AudioClip failSound;
-	    public AudioClip successSound;
-	    public AudioClip gameOverSound;
-    // Animations
-	    private Animator letterBoxController;
-		private Animator eventLetterAnim; //------------------------------will control the animations of the what is 
+    // Declarations and Initializations
+    // ---------------------------------
+        // Cached integer from the actor
+            private int cacheNumber;
     // ----
 
 
 
-    // ReadOnly Accessors; should the spawner's be activated?
-    public bool ActivateSpawner
+    // During the collision, cache the actor's uniquely assigned 
+    private void OnTriggerEnter (Collider actor)
     {
-        // thanks to Bob for giving me the idea in his previous code.
-        get { return activateSpawner; }
-    } // End of ActivateSpawner
+        // Fetch the number from the actor
+            cacheNumber = RetrieveActorIdentity(actor);
+        // Destroy the actor
+            Destroy(actor.gameObject);
+    } // OnTriggerEnter()
 
 
 
-	void Awake()
-	{
-		eventLetterAnim = whatIsText.GetComponent<Animator>(); // finds the whatis text G.O. and gets the animator.
-	}
-
-    // Initialize the declarations
-    void Start()
+    // This function is designed to fetch the uniquely self-assigned number.
+    private int RetrieveActorIdentity(Collider actorObject)
     {
-        // Turn on the spawner's
-        activateSpawner = true;
-        // Turn off the lock
-        lockCheckFunction = false;
-        // Reference initialization
-		letterBoxController = letterBox.GetComponent<Animator>();
-    } // End of Start
+        // Fetch the minion's unique script.
+            CreatureIdentity tempData = actorObject.gameObject.GetComponent<CreatureIdentity>();
+        // Fetch and return the minion's uniquely assigned number.
+             return (tempData.MinionNumber);
+    } // RetrieveActorIdentity()
 
 
 
-    // When the object hits the object, run the check.
-    void OnTriggerEnter(Collider other)
+    // This accessor will allow outside classes to retrieve the actor's number.
+    public int ActorIdentity
     {
-        if (lockCheckFunction == false)
-        {
-            // Fetch the minion's uniquely assigned number; this will be used for comparison.
-            CreatureIdentity id = other.gameObject.GetComponent<CreatureIdentity>();
-
-            // If the answer is correct
-            if (GetNumber() == id.MinionNumber)
-                // Answer is correct
-                CorrectAnswer();
-            else
-                // Answer is not correct
-                IncorrectAnswer();
-        } // End If
-
-        // Destroy the game object when they activate this trigger
-        Destroy(other.gameObject);
-    } // End of OnTriggerEnter
-
-
-
-    // When invoked, this will return the current Quadratic index used in the Letter Box (from Canvas)
-    int GetNumber()
-    {
-        switch (letterBox.text)
-        {
-            case "A":
-                return problemBox.Index_A;
-            case "B":
-                return problemBox.Index_B;
-            default:
-                return problemBox.Index_C;
-        }
-    } // End of GetNumber
-
-
-
-    // When the player has the right answer, this function will house the algorithm
-    private void CorrectAnswer()
-    {
-		// Plays the 'successSound' if the right minion goes through [DC]
-		GetComponent<AudioSource>().clip = successSound;
-		GetComponent<AudioSource>().Play ();
-		// ----
-
-        // Update the score
-        score.AccessUpdateScoreCorrect();
-        // ----
-
-        // Stop the spawner's
-        activateSpawner = false;
-
-        // Lock the exit from further comparison
-        lockCheckFunction = true;
-        // ----
-
-
-        // Murder the minions!
-        MinionGenocide();
-        // ----
-
-
-        // Check the game state to see if the game is over or still on going
-        StartCoroutine(waitTimer_GameStateCheck(1));
-        // ----
-
-
-        // Timer to unlock the 'Lock Check Function' variable
-        StartCoroutine(WaitTimer(waitTimer + 1));
-
-		eventLetterAnim.SetTrigger ("SlideIn");
-        // ----
-
-    } // End of CorrectAnswer
-
-
-
-    // Once activated; this halts the action within the function before executing anything further.
-    private IEnumerator WaitTimer(int waitTime)
-    {
-        // Waits for $waitTime seconds
-        yield return new WaitForSeconds(waitTime);
-        // Unlock the function [OnTriggerEvent]
-        lockCheckFunction = false;
-        // Resume the minion spawner's
-        activateSpawner = true;
-    } // End of WaitTimer
-
-
-
-    // Pause the script to wait for the game over state to catch up
-    private IEnumerator waitTimer_GameStateCheck(int waitCheck)
-    {
-        yield return new WaitForSeconds(waitCheck);
-
-        // If the game is not over, continue on generating a new equation and the rest of the algorithm
-        if (gameState.GameStateOver == false)
-        {
-            // Generate a new equation
-            problemBox.Access_Generate();
-            letterbox.Access_Generate();
-            // ----
-
-
-            // Notify the user of index update
-            letterBoxController.SetTrigger("LetterChange");
-            whatIsText.GetComponent<Animation>().Play();
-        } // End If
-    } // End of Pause_GameOverCheck
-
-
-
-    // When the user has the incorrect answer, this function will manage the routines
-    private void IncorrectAnswer()
-    {
-        // Update the incorrect score
-        score.AccessUpdateScoreIncorrect();
-
-		// Plays the 'failSound' if the wrong minion goes through [DC]
-		GetComponent<AudioSource>().clip = failSound;
-		GetComponent<AudioSource>().Play ();
-		// ----
-
-    } // End of IncorrectAnswer
-
-
-
-    // Kill all minions in the scene
-    private void MinionGenocide()
-    {
-        // Run a detection of the minions in the scene
-        if (minionSpawnWait.CheckMinions() == true)
-        {
-            // Fetch all of the minions in one array
-            GameObject[] minionsInScene = GameObject.FindGameObjectsWithTag("Minion");
-            // Kill them
-            for (int i = 0; i < minionsInScene.Length; i++)
-                DestroyObject(minionsInScene[i]);
-        } // End If
-
-    } // End of MinionGenocide
-
-
-
-    // As the 'MinionGenocide' is a private function, kindly just call it for the outside scripts.
-    public void AccessMinionGenocide()
-    {
-        MinionGenocide();
-    } // End of AccessMinionGenocide
-
+        get { return cacheNumber; }
+    } // ActorIdentity
 } // End of Class
