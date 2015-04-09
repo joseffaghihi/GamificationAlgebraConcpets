64244b4bab839ce2aeea0cc55d348822c853beeb
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2015-04-08 23:49:50 -0500

    Select an actor when they touch each other
    When the minion collides with another, this code will randomly selects a
    minion that'll be thrown off the ladder (or stays in the ladder, this
    code doesn't choose that condition.).

--- Assets/MinionMathMayhem/Scripts/Ship/Minion_Controller.cs
+++ Assets/MinionMathMayhem/Scripts/Ship/Minion_Controller.cs
@@ -89,5 +89,69 @@ namespace MinionMathMayhem_Ship
         {
             get { return thrustDirection; }
         } // ThrustDirection
+
+
+
+        // Additional Member Variables
+        //   These variables will be used for avoiding cross-values because when Actor2 touches Actor1, 
+        //    both actors will execute the collision code, thus - we allow the Actor2 and Actor1 to call
+        //    MinionCollision(), but disallow Actor1 and Actor2 from calling the function.
+        //    Elaborative:
+        //     ACTOR2 & ACTOR1 -> MinionCollision()
+        //     ACTOR1 & ACTOR2 -> (DISALLOW) MinionCollision()
+            private int minionCollision_Actor1;
+            private int minionCollision_Actor2;
+        // Minion Collision; determine which one is the alpha-male!
+        public int MinionCollision(int minionActor1, int minionActor2)
+        {
+            // Avoid cross-values
+            if (MinionCollision_CrossValues(minionActor1, minionActor2) == false)
+                // Select the minion that will be thrown off the ladder
+                    return MinionCollision_Process(minionActor1, minionActor2);
+            else
+                return 0; // POSSIBLE BUG!  If the actor's instance ID is 0, it'll be selected.  [NG]
+        } // MinionCollision()
+
+
+
+        // Determine which minion is going to be thrown off the ladder 
+        private int MinionCollision_Process(int actor1, int actor2)
+        {
+            // Update the member variables
+                minionCollision_Actor1 = actor1;
+                minionCollision_Actor2 = actor2;
+            // Clear the member variables once their values become irrelevant.
+                StartCoroutine(MinionCollision_ThrashValues());
+
+            // randomly select the minion that'll be thrown off
+            if (System.Convert.ToBoolean(UnityEngine.Random.Range(0, 2)))
+                return actor1;
+            else
+                return actor2;
+        } // MinionCollision_Process()
+
+
+
+        // Check to make sure that the other actor is not reporting the same values. 
+        private bool MinionCollision_CrossValues(int actor1, int actor2)
+        {
+            if (actor1 == minionCollision_Actor1)
+                // Cross-Value detected
+                return true;
+            else
+                return false;
+        } // MinionCollision_CrossValues()
+
+
+
+        // After so long, thrash the values as they'll become irrelevant
+        private IEnumerator MinionCollision_ThrashValues()
+        {
+            // Wait before thrashing
+                yield return new WaitForSeconds(0.3f);
+            // Thrash the values
+                minionCollision_Actor1 = 0;
+                minionCollision_Actor2 = 0;
+        } // MinionCollision_ThrashValues()
     } // End of Class
 } // namespace
