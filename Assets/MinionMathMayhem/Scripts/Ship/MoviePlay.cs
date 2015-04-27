d58ceaa1a4089fb7dbf6b56157a974b10ad31196
SibTiger <Nicholas.Gautier.Tiger@GMail.com>
2015-04-27 00:41:33 -0500

    Applied Namespace
    Added namespace to the script file.

--- Assets/MinionMathMayhem/Scripts/Ship/MoviePlay.cs
+++ Assets/MinionMathMayhem/Scripts/Ship/MoviePlay.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoviePlay : MonoBehaviour {
+namespace MinionMathMayhem_Ship
+{
+    public class MoviePlay : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		Renderer r = GetComponent<Renderer>();
-		MovieTexture movie = (MovieTexture)r.material.mainTexture;
-		movie.Play ();
-	}
+        // Use this for initialization
+        void Start()
+        {
+            Renderer r = GetComponent<Renderer>();
+            MovieTexture movie = (MovieTexture)r.material.mainTexture;
+            movie.Play();
+        }
+    }
 }
