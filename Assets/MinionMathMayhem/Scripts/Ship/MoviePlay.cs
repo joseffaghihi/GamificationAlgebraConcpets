f36678af406e6264a7af335cdb102e9e4cb82777
SibTiger <Nicholas.Gautier.Tiger@GMail.com>
2015-04-27 01:33:48 -0500

    Expanded behavior [WIP]
    Added the ability to detect if the movie is still playing.
    Added documentation.

--- Assets/MinionMathMayhem/Scripts/Ship/MoviePlay.cs
+++ Assets/MinionMathMayhem/Scripts/Ship/MoviePlay.cs
@@ -5,13 +5,53 @@ namespace MinionMathMayhem_Ship
 {
     public class MoviePlay : MonoBehaviour
     {
+        /*              Tutorial Movie
+         * Plays the tutorial movie specifically for the map.  This script will play the desired movie and send a signal that the movie has ended.
+         * 
+         * 
+         * Dependencies:
+         *      QuickTime
+         * 
+         * Goals:
+         *      Play the tutorial movie
+         *      Report back when the tutorial has finished.
+         */
 
-        // Use this for initialization
-        void Start()
+
+
+        // Start-Up execution
+        private IEnumerator Start()
         {
-            Renderer r = GetComponent<Renderer>();
-            MovieTexture movie = (MovieTexture)r.material.mainTexture;
-            movie.Play();
-        }
+            // Initialization Field
+                Renderer r = GetComponent<Renderer>();
+                MovieTexture movie = (MovieTexture)r.material.mainTexture;
+            // ----
+
+            // Play the movie
+                Movie_Execute(movie);
+            // ----
+
+            // Wait for the movie to end
+                yield return (StartCoroutine(Movie_Ended_Check(movie)));
+            // ----                
+        } // Start()
+
+
+        // Play the movie
+        private void Movie_Execute(MovieTexture onScreenMovie)
+        {
+            onScreenMovie.Play();
+        } // ExecuteMovie()
+
+
+        private IEnumerator Movie_Ended_Check(MovieTexture onScreenMovie)
+        {
+            do
+            {
+                yield return new WaitForSeconds(.5f);
+            } while (onScreenMovie.isPlaying == !false);
+
+            yield return null;
+        } // Movie_Ended_Check();
     }
 }
