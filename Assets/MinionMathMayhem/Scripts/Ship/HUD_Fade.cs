827309aede1e7c29a8a2a56cfda0c51b1accdbbe
Nicholas "Tiger" Gautier <SibTiger_And_Life@Hotmail.com>
2015-06-03 08:23:43 -0500

    Added HUD Fader script [WIP]
    This script goals are to change the transparency levels of the HUD in
    the given events.  For example, the HUD should be invisible when the
    tutorial is running or when the game is over.

---
+++ Assets/MinionMathMayhem/Scripts/Ship/HUD_Fade.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace MinionMathMayhem_Ship
+{
+    public class HUD_Fade : MonoBehaviour
+    {
+        /*
+         *              HEADS UP DISPLAY FADE
+         *
+         * This class will simply have the HUD fade in and fade out depending on the situation.  The idea of this feature is to clear the screen 
+         * when an event is happening.  An example of an event, tutorial, game over, and the like.
+         * 
+         * DEPENDENCY OBJECT NOTES:
+         *      HUD_Fade
+         *          |_ Score
+         *          |_ Problem Box
+         *          |_ Letter
+         *          
+         * GOALS:
+         *      * Clear the HUD off the screen by fading it out
+         *          The objects are NOT removed from the world!
+         *      * Bring back the HUD on the screen by fading it back in.
+         *
+         */
+
+
+        // Declarations and Initializations
+        // ---------------------------------
+            // Normal Fade Target [when the game is playing]
+                private float alphaChannelNormal;
+            // Hide Fade Target [Fade out]
+                public float alphaChannelHide = 0.0f;
+            // Speed of fader
+                public float alphaChangeSpeed = 0.0f;
+        // ---------------------------------
+
+
+
+
+        // Immediately execute when the game object is available within the scene.
+        private void Start()
+        {
+            // Make sure that the mutable variables do not contain negative values.
+                CheckValues();
+            // Initialize the default fade level with the desired current alpha value.
+                alphaChannelNormal = gameObject.GetComponent<CanvasGroup>().alpha;
+        } // Start()
+
+
+
+        // Prevent negative values
+        private void CheckValues()
+        {
+            if (alphaChannelHide < 0)
+                alphaChannelHide = (alphaChannelHide * -1);
+            if (alphaChangeSpeed < 0)
+                alphaChangeSpeed = (alphaChangeSpeed * -1);
+            if (alphaChannelNormal < 0) // Added this just incase it's possible to add negative values within Unity's inspector.
+                alphaChannelNormal = (alphaChannelNormal * -1);
+        } // CheckValues()
+    } // End of Class
+} // Namespace
