fd7d5bcafd1a712f36220ebd6054361a57dee995
SibTiger <Nicholas.Gautier.Tiger@GMail.com>
2015-06-18 01:55:25 -0400

    Revised documentation.

--- Assets/MinionMathMayhem/Scripts/Ship/TutorialSkipButton.cs
+++ Assets/MinionMathMayhem/Scripts/Ship/TutorialSkipButton.cs
@@ -5,24 +5,26 @@ namespace MinionMathMayhem_Ship
 {
     public class TutorialSkipButton : MonoBehaviour
     {
+
         /*                    TUTORIAL SKIP BUTTON
-         * This script, being simple, allows the player to skip the entire tutorial.  This is done by sending a signal to the tutorial to 'stop' its execution.
+         * This script, being simple, allows the player to skip the entire tutorial sequence.
+         *      This is done by sending a broadcast signal (or event) to abort or stop the tutorial sequence.
          *  
          * GOALS:
-         *  When activated (or true), stop the tutorial and start the game or the next part of the game.
+         *  When activated (or true), stop the tutorial sequence and start the game or the next part of the game.
          */
 
 
         // Declarations and Initializations
         // ---------------------------------
-        // Accessors and Communication
-            public delegate void SkipTutorial();
-            public static event SkipTutorial SkipTutorialDemand;
+            // Accessors and Communication
+                public delegate void SkipTutorial();
+                public static event SkipTutorial SkipTutorialDemand;
         // ----
 
 
 
-        // When the user clicks on the object, the object will begin to be disabled from the virtual world -- this function will automatically execute during this phase.
+        // When the object is preparing to be removed from the virtual world; broadcast a signal (event) that the tutorial squence needs to be skipped.
         private void OnDisable()
         {
             SkipTutorialDemand();
