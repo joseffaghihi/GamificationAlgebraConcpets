4ea6db9447a918a571ef8fc61e88142f08d88a8e
Davidizm <skullmaru@live.com>
2015-09-11 21:20:36 -0500

    Added a few comments
    1. Added some comments

--- Assets/MinionMathMayhem/Scripts/Ship/FeedbackAnswer.cs
+++ Assets/MinionMathMayhem/Scripts/Ship/FeedbackAnswer.cs
@@ -7,21 +7,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class FeedbackAnswer : MonoBehaviour {
-	private Text text;
-	private Text answerText;
+namespace MinionMathMayhem_Ship
+{
+    public class FeedbackAnswer : MonoBehaviour
+    {
+        private Text text;
+        // This is for supplying the fb_answer with the right number
+        // But it doesn't work yet // -------DC------- // 9/11/15
+        // private Text answerText;
 
-	// This variable is a reference to the A,B, or C textbox
-	public GameObject answerObject;
+        // This variable is a reference to the A,B, or C textbox
+        public GameObject answerObject;
 
-	// Use this for initialization
-	void Start () {
-		text = GetComponent<Text>();
-		answerText = answerObject.GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		text = answerText;
-	}
-}
+        // Use this for initialization
+        void Start()
+        {
+            text = GetComponent<Text>();
+            // answerText = answerObject.GetComponent<Text>(); // -------DC------- // 9/11/15
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // text = answerText; // -------DC------- // 9/11/15
+        }
+    } // class
+} // namepace
