993511080c836a78353ed92835683bce279756fa
WestonRWright <wrightw@uindy.edu>
2015-10-22 22:17:32 -0400

    Imported project
    Imported project to Unity github.

---
+++ Assets/Prime Numbers/Scripts/DeleteCube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteCube : MonoBehaviour {
+
+	public TextMesh CubeText;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+			if (transform.position.y < -20) 
+			{
+				Destroy (gameObject);
+			}
+		}
+}
