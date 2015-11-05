904ef0665ae5bef1bfb2f47e0f2af40c1ed129bc
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2015-11-05 14:20:36 -0600

    [WIP] Revising the tutorial structure
    Revising how the tutorials are organized.  This is very much WIP

---
+++ Assets/MinionMathMayhem/Scripts/Ship/Tutorial/Base Scripts/TutorialMovie_GeneralScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MinionMathMayhem_Ship
+{
+    public class TutorialMovie_GeneralScript : MonoBehaviour
+    {
+        /*                                          TUTORIAL MOVIE [GENERAL SCRIPT]
+         * This class is designed to offer a standard base as to how the movies will work with the tutorial protocol.
+         *
+         * GOALS:
+         *  Initalizations for the specific movie object
+         */
+
+        // Declarations and Initializations
+        // ---------------------------------
+            public GameObject objectTutorial_Movie, objectTutorial_Canvas, objectTutorial_SkipButton;
+        // ---------------------------------
+    } // End of Class
+} // Namespace
---
+++ Assets/MinionMathMayhem/Scripts/Ship/Tutorial/Base Scripts/TutorialWindow_GeneralScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MinionMathMayhem_Ship
+{
+    public class TutorialWindow_GeneralScript : MonoBehaviour
+    {
+        /*                                          TUTORIAL WINDOW [GENERAL SCRIPT]
+         * This class is designed to offer a standard base as to how the dialog windows will work with the tutorial protocol.
+         *
+         * GOALS:
+         *  Initalizations for the specific dialog window object
+         */
+
+        // Declarations and Initializations
+        // ---------------------------------
+            public GameObject rulesCanvas;
+            //  Rules Control on the rulesCanvas GameObject  
+                private RulesControl rulesControl;
+        // ---------------------------------
+
+        
+        
+        /// <summary>
+        /// Unity function
+        /// When this object is active, this function will be called automatically.
+        /// </summary>
+        private void Awake()
+        {
+            rulesControl = rulesCanvas.GetComponent<RulesControl>();
+        } // Awake()
+    } // End of Class
+} // Namespace
