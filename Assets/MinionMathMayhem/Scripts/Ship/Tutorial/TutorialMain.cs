88b41851d402c7466e33d76fff7af4f601a424be
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2015-11-04 23:56:01 -0600

    [WIP] Added TutorialMain script
    This script will be used to keep the tutorial functionality and
    sequences organized and easier to manage.
    NOTE: Very much WIP

---
+++ Assets/MinionMathMayhem/Scripts/Ship/Tutorial/TutorialMain.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MinionMathMayhem_Ship
+{
+    public class TutorialMain : MonoBehaviour
+    {
+        /// <summary>
+        ///     Unity Function
+        ///     Signal Listener: Detected (or heard)
+        /// </summary>
+        private void OnEnable()
+        {
+
+        }
+
+
+
+        /// <summary>
+        ///     Unity Function
+        ///     Signal Listener: Deactivated
+        /// </summary>
+        private void OnDisable()
+        {
+
+        }
+
+
+
+        /// <summary>
+        ///     Main tutorial sequence driver that manages how the tutorials are to be displayed and what type.
+        /// </summary>
+        /// <param name="tutorialMovie">
+        ///     When true, this will display a movie [can be concurrent with tutorialWindow].  Default is false.
+        /// </param>
+        /// <param name="tutorialWindow">
+        ///     When true, this will display a window [can be concurrent with tutorialMovie].  Default is false.
+        /// </param>
+        /// <param name="randomSwitch">
+        ///     When true, this will randomize what tutorials (movie and/or window) is to be played; if part of the index array.  Default is false.
+        /// </param>
+        /// <param name="tutorialCategory">
+        ///     Select an index category for which to play or display the tutorial.  Default is 0.
+        /// </param>
+        /// <param name="PlayIndex">
+        ///     Forcibly play or display the window within the exact index.  Default is 0.
+        /// </param>
+        private void TutorialMain_Driver(bool tutorialMovie = false,
+                                        bool tutorialWindow = false,
+                                        bool randomSwitch = false,
+                                        uint tutorialCategory = 0,
+                                        int PlayIndex = 0)
+        {
+
+        }
+
+
+    }
+}
