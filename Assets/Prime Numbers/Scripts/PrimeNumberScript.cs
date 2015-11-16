4c7ef1324df21d7b6d10bdcaf673d95e66321317
WestonRWright <wrightw@uindy.edu>
2015-11-16 12:53:36 -0500

    Replacing cubes with dynamites.
    Replacing cubes with dynamites.

--- Assets/Prime Numbers/Scripts/PrimeNumberScript.cs
+++ Assets/Prime Numbers/Scripts/PrimeNumberScript.cs
@@ -11,7 +11,7 @@ namespace PrimeNumbers
 		
 		private void Awake()
 		{
-			StartNumber_script = GameObject.Find("StartCube").GetComponentInChildren<StartNumber>();
+			StartNumber_script = GameObject.Find("Start Dynamite").GetComponentInChildren<StartNumber>();
 		}
 		
 		void Update ()
