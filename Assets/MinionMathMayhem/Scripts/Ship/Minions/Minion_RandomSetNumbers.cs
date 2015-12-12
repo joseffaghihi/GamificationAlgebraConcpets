ab9774d467482bbde0fef51e33cf485e956e4357
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2015-12-11 19:54:58 -0600

    [WIP]  Control Random Number for Minions
    Control the numbers within an array to make the game a bit more
    challenging and not complete bordem.

---
+++ Assets/MinionMathMayhem/Scripts/Ship/Minions/Minion_RandomSetNumbers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MinionMathMayhem_Ship
+{
+    public class Minion_RandomSetNumbers : MonoBehaviour
+    {
+        /*                          MINION RANDOM SET NUMBERS
+         *  This class is designed to generate a specific set of numbers that each minion will be assigned.
+         *   This will help to control when the 'right' answer appears to the user -- without this
+         *   enforcement control and due to randomness of generated at time, time will elapse of when the
+         *   user is bored and when the answer appears.  This will control what numbers each minions will have
+         *   and try to make it challenging to the user if needed or easier.
+         *
+         *
+         * STRUCTURAL DEPENDENCY NOTES:
+         *      Quadratic Equation TextBox
+         *           |_ Problem Box
+         *      Game Controller
+         *           |_ GameEvent
+         *
+         *
+         * GOALS:
+         *  Fetch the number ranges
+         *  Fetch for the expected answer
+         *  Randomize the entire number set
+         *  Place the right answer in a random index
+         *  Regenerate when needed
+         */
+
+        #region Declaration and Initializations
+        // Number set array
+            private int[] numberSetArray = new int[20];
+
+        #endregion
+    }
+}
