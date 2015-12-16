639097cd8e42949d8f6fe7113bad08febb36cd58
WestonRWright <wrightw@uindy.edu>
2015-12-16 13:24:55 -0500

    Change variable names
    1. Changed variable names to be more descriptive
    2. Edited some prefabs (Five Dynamites, TestSpawn5)

---
+++ Assets/Prime Numbers/Scripts/DynamiteSpawnTestText.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PrimeNumbers
+{
+	public class DynamiteSpawnTestText : MonoBehaviour 
+	{
+
+		public TextMesh text;
+		public int Nr1;
+
+		private void Awake () 
+		{
+			text = GetComponent<TextMesh>();
+		}
+
+		void Start () 
+		{
+			StartCoroutine (initNr1());
+				
+			if (!isPrime (Nr1)) 
+			{
+			}
+		}
+	
+		IEnumerator initNr1() 
+		{
+			while(Nr1 == 0)
+				yield return null;
+
+			text.text = Nr1.ToString ();
+			print ("Nr1 initialized.");
+		}
+
+		public bool isPrime(int Nr1)
+		{
+			for(int i=2; i<Nr1;i++)
+			{
+				if(Nr1%i==0)
+					return false;
+			}
+			return true;
+		}
+	}
+}
